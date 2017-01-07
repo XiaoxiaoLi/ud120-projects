952df5dc8dbae5cd26948d8b7f1f15e8a26dfaaf
XiaoxiaoLi <xiaomao@Damaos-MBP.lan>
2017-01-07 14:44:29 -0500

    Decision Tree quiz

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -7,12 +7,12 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -20,12 +20,25 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
 
+def get_decision_tree_prediction_accuracy(features_train, features_test, labels_train, labels_test):
+    from sklearn import tree
 
+    clf = tree.DecisionTreeClassifier(min_samples_split=40)
+    clf = clf.fit(features_train, labels_train)
+
+    ypred = clf.predict(features_test)
+
+    from sklearn.metrics import accuracy_score
+
+    return accuracy_score(labels_test, ypred)
+
+
+def get_feature_num(training_set):
+    return len(training_set[0])
+
+
+print get_decision_tree_prediction_accuracy(features_train, features_test, labels_train, labels_test)
 #########################################################
-
-
