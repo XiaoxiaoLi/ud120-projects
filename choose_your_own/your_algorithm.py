376f28ed3b8ce6935273ee6fc0b45b20fe347de6
XiaoxiaoLi <xiaomao@Damaos-MBP.lan>
2017-01-07 15:48:55 -0500

    Choose your own algorithm, not able to beat

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -31,14 +31,39 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+def get_ada_boost_classifier():
+    from sklearn.ensemble import AdaBoostClassifier
+    return AdaBoostClassifier(n_estimators=50, learning_rate=0.5)
 
 
+def get_random_forest_classifier():
+    from sklearn.ensemble import RandomForestClassifier
+    return RandomForestClassifier(max_depth=5)
 
 
+def get_knn_classifier():
+    from sklearn.neighbors import NearestNeighbors
+    return NearestNeighbors.KNeighborsClassifier()
 
 
+def split_train_validate(features_train,labels_train):
+    from sklearn.model_selection import train_test_split
+    return train_test_split(features_train, labels_train, test_size = 0.3, random_state = 42)
+
+features_train, features_validate, labels_train, labels_validate = split_train_validate(features_train,labels_train)
+
+clf = get_random_forest_classifier()
+clf = clf.fit(features_train, labels_train)
+
+ypred = clf.predict(features_validate)
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(labels_validate, ypred)
+
+# print accuracy_score(labels_test, ypred)
+#
 
 try:
-    prettyPicture(clf, features_test, labels_test)
+    prettyPicture(clf, features_validate, labels_validate)
 except NameError:
     pass
