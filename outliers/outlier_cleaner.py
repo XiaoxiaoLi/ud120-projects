03dca2a3d3ae6c1e79bd7c5b31f7f4d5484796d1
cindy.xiaoxiao.li <cindyxiaoxiaoli@gmail.com>
2017-04-01 19:18:26 -0400

    quiz for outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import numpy as np
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -12,9 +12,9 @@ def outlierCleaner(predictions, ages, net_worths):
     """
     
     cleaned_data = []
-
-    ### your code goes here
-
+    errors = np.array(predictions[:,0]) - np.array(net_worths[:,0])
+    sorted_errors_indices = np.argsort(errors)
+    bottom_error_indices = sorted_errors_indices[:int(len(sorted_errors_indices)*0.9)]
+    cleaned_data = [(ages[i], net_worths[i], errors[i]) for i in bottom_error_indices]
     
     return cleaned_data
-
