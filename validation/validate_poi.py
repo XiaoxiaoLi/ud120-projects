1f3266ad0028b90b00bf3a43e0400442b5ea53ce
cindy.xiaoxiao.li <cindyxiaoxiaoli@gmail.com>
2017-04-16 12:33:27 -0400

    Add validation quiz

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -28,5 +28,12 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
-
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+from sklearn.metrics import accuracy_score
+accuracy_score(y_test, y_pred)
